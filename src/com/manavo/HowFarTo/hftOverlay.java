ed32138ed1f7548392708398e677971faacd5662
manavo <philippakis@yahoo.com>
2011-08-30 11:36:24 +0100

    Show location on map with sample icon, and show alert dialog with options if there is more than one returned

---
+++ src/com/manavo/HowFarTo/hftOverlay.java
@@ -0,0 +1,53 @@
+package com.manavo.HowFarTo;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class hftOverlay extends ItemizedOverlay {
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private Context mContext;
+	
+	public hftOverlay(Drawable defaultMarker) {
+		super(boundCenterBottom(defaultMarker));
+	}
+	
+	public hftOverlay(Drawable defaultMarker, Context context) {
+		super(boundCenterBottom(defaultMarker));
+		mContext = context;
+	}
+	
+	public void clear() {
+		this.mOverlays.clear();
+	}
+	
+	@Override
+	protected boolean onTap(int index) {
+		OverlayItem item = mOverlays.get(index);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+		dialog.setTitle(item.getTitle());
+		dialog.setMessage(item.getSnippet());
+		dialog.show();
+		return true;
+	}
+	
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
+	}
+	
+	@Override
+	protected OverlayItem createItem(int i) {
+	  return mOverlays.get(i);
+	}
+	
+	@Override
+	public int size() {
+	  return mOverlays.size();
+	}
+}
--- src/com/manavo/HowFarTo/main.java
+++ src/com/manavo/HowFarTo/main.java
@@ -1,43 +1,111 @@
 package com.manavo.HowFarTo;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class main extends Activity {
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+public class main extends MapActivity {
+	private MapView mapView;
+	
+	private List<Overlay> mapOverlays;
+	private Drawable drawable;
+	private hftOverlay itemizedoverlay;
+	
+	private List<Address> addresses;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        this.mapView = (MapView)this.findViewById(R.id.mapview);
+        this.mapView.setBuiltInZoomControls(true);
+        
+        mapOverlays = mapView.getOverlays();
+        drawable = this.getResources().getDrawable(R.drawable.icon);
+        itemizedoverlay = new hftOverlay(drawable, this);
+/*        
+        GeoPoint point2 = new GeoPoint(35410000, 139460000);
+        OverlayItem overlayitem2 = new OverlayItem(point2, "Sekai, konichiwa!", "I'm in Japan!");
+        itemizedoverlay.addOverlay(overlayitem2);*/
+    }
+    
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+    
+    public void showAddress(Address address) {
+    	Integer lat = new Double(address.getLatitude()*1E6).intValue();
+		Integer lng = new Double(address.getLongitude()*1E6).intValue();
+		
+		mapOverlays.clear();
+        
+        GeoPoint point = new GeoPoint(lat, lng);
+        OverlayItem overlayitem = new OverlayItem(point, "Hello!", "I'm in "+address.getAddressLine(0)+"!");
+        
+        itemizedoverlay.clear();
+        itemizedoverlay.addOverlay(overlayitem);
+
+        mapOverlays.add(itemizedoverlay);
+        
+		mapView.invalidate();
     }
     
     public void searchLocation(View v) {
     	EditText location = (EditText)this.findViewById(R.id.location);
-    	TextView result = (TextView)this.findViewById(R.id.result);
+    	//TextView result = (TextView)this.findViewById(R.id.result);
     	
     	String sLocation = location.getText().toString();
     	
     	Geocoder g = new Geocoder(this);
     	try {
-			List<Address> addresses = g.getFromLocationName(sLocation, 5);
-			Iterator<Address> i = addresses.iterator();
-			Address a;
-			String sResult = "";
-			while (i.hasNext()) {
-				a = i.next();
-				sResult += a.getAddressLine(0)+"\n";
+			addresses = g.getFromLocationName(sLocation, 5);
+			
+			if (addresses.size() == 1) {
+				showAddress(addresses.get(0));
+			} else if (addresses.size() == 0) {
+				Toast.makeText(this, "Nothing found!", Toast.LENGTH_LONG).show();
+			} else {
+				Iterator<Address> i = addresses.iterator();
+				Address a;
+				
+				ArrayList<String> options = new ArrayList<String>();
+				while (i.hasNext()) {
+					a = i.next();
+					options.add(a.getAddressLine(0) + ", " + a.getCountryName());
+				}
+				CharSequence[] cs = options.toArray(new CharSequence[options.size()]);
+
+				AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+				dialog.setItems(cs, new OnClickListener() {
+					public void onClick(DialogInterface arg0, int index) {
+						showAddress(main.this.addresses.get(index));
+					}
+				});
+				dialog.show();
 			}
-			result.setText(sResult);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
